babaa2bd9fab49e5fdd470b817285456c0531e29
MasindeOdera <MasindeOdera@gmail.com>
2020-05-22 15:23:53 +0200

    Updates

--- precache-manifest.0f29256e342e8a642288151d3452e4a6.js
+++ precache-manifest.0f29256e342e8a642288151d3452e4a6.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0df142fcb95ad0f01a288e4a3b648331",
+    "revision": "735dd3d7d1b908e497285120a4fcf5d5",
     "url": "/the-powerpuff-girls/index.html"
   },
   {
-    "revision": "4316fb090794f6878000",
-    "url": "/the-powerpuff-girls/static/css/main.1568ec30.chunk.css"
+    "revision": "4fbdb9b9196a3f69c282",
+    "url": "/the-powerpuff-girls/static/css/main.07073fb8.chunk.css"
   },
   {
     "revision": "118f681aedc44c2be02c",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/the-powerpuff-girls/static/js/2.6760b9e4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "4316fb090794f6878000",
+    "revision": "4fbdb9b9196a3f69c282",
     "url": "/the-powerpuff-girls/static/js/main.0302cf4f.chunk.js"
   },
   {
