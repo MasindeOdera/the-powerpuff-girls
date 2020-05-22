64563e9866fbb2de243cd3ac1374e6a80ff38bf5
MasindeOdera <MasindeOdera@gmail.com>
2020-05-22 13:20:35 +0200

    Added NavigationHeader component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import NavigationHeader from './components/NavigationHeader';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <NavigationHeader />
       <header className="App-header">
         <h3>The Powerpuff Girls</h3>
       </header>
---
+++ src/components/NavigationHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavigationHeader.css';
+
+function NavigationHeader() {
+    return (
+        <div className="Navigate">
+            <h4>Home / Shows / The Powerpuff Girls</h4>
+        </div>
+    )
+}
+
+export default NavigationHeader
