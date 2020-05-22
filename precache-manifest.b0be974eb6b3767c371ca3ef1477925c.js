ba9c9f121d061f2936b66d32d87d1ce005015843
MasindeOdera <MasindeOdera@gmail.com>
2020-05-22 11:23:44 +0200

    Updates

---
+++ precache-manifest.b0be974eb6b3767c371ca3ef1477925c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53a3e80dde6a1a3672509d9224c92a4d",
+    "url": "/the-powerpuff-girls/index.html"
+  },
+  {
+    "revision": "ce8ebf1dbbe8e6e14307",
+    "url": "/the-powerpuff-girls/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "09405993c999a0142a60",
+    "url": "/the-powerpuff-girls/static/js/2.73a757d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/the-powerpuff-girls/static/js/2.73a757d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce8ebf1dbbe8e6e14307",
+    "url": "/the-powerpuff-girls/static/js/main.5a5d7e62.chunk.js"
+  },
+  {
+    "revision": "f7a722fbaf6fdb23a101",
+    "url": "/the-powerpuff-girls/static/js/runtime-main.29c1325a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/the-powerpuff-girls/static/media/logo.5d5d9eef.svg"
+  }
+]);
