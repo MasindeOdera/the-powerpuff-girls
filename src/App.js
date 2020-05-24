3f33bd71471c0497d71dd7ffd613f341a78028f8
MasindeOdera <MasindeOdera@gmail.com>
2020-05-24 10:51:47 +0200

    Updated Main.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import PathHeader from './components/PathHeader';
 import Navigation from './components/Navigation';
 import Main from './components/Main';
@@ -9,22 +9,6 @@ function App() {
 
   const counter = useSelector( (state) => state.counter );
   const dispatch = useDispatch();
-
-  const [initialData, setInitialData] = useState('');
-
-  useEffect(() => {
-    fetch('http://api.tvmaze.com/singlesearch/shows?q=the-powerpuff-girls')
-      .then(res => res.json())
-      .then(data => {
-        console.log({data});
-        setInitialData(data);
-      });
-  }, []);
-
-  useEffect(() => {
-    dispatch({type:"DISPLAY", payload: initialData})
-  });
-
 
   return (
     <div className="App">
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Item } from 'semantic-ui-react';
-import { useSelector} from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
 import placeholder from '../images/placeholder.png';
 
 function Main() {
-    const initialData = useSelector( (state) => state.initialData);
+    const [initialData, setInitialData] = useState('');
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+      fetch('http://api.tvmaze.com/singlesearch/shows?q=the-powerpuff-girls')
+        .then(res => res.json())
+        .then(data => {
+          console.log({data});
+          setInitialData(data);
+        });
+    }, []);
+  
+    useEffect(() => {
+      dispatch({type:"DISPLAY", payload: initialData})
+    });
+
+    // const initialData = useSelector( (state) => state.initialData);
     const image = useSelector( (state) => state.initialData.image);
-    // const description = useSelector( (state) => state.description);
+
     console.log("State from Main.js: ", {initialData});
     console.log(initialData.image);
     // console.log(Object.keys(image)[0]);
@@ -14,7 +30,7 @@ function Main() {
         <div>
             <Item.Group>
             <Item>
-                <Item.Image size='small' src={image ? image.medium : placeholder} />
+                <Item.Image size='small' src={image !== undefined ? image.medium : placeholder} />
                 <Item.Content header='Cute Dog' description={initialData.summary} />
             </Item>
             </Item.Group>
