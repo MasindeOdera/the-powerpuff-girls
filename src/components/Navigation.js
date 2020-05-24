3b4adebe978fceddbe40c6329afa4e5af43b942f
MasindeOdera <MasindeOdera@gmail.com>
2020-05-24 12:11:39 +0200

    Added onClick to button, Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import { useSelector} from 'react-redux';
+import { useSelector, useDispatch} from 'react-redux';
 import './Navigation.scss';
 
 function Navigation() {
     const state = useSelector( (state) => state);
+    const dispatch = useDispatch();
     console.log("State from Navigation: ", state);
     return (
         <div className="Navigation">
-            <button><b>Main</b></button>
+            <button  onClick={()=> dispatch({type:"DISPLAY", payload: state.initialData})}><b>Main</b></button>
             <button><b>Episodes</b></button>
         </div>
     )
