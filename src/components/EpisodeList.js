242d3ba32d123b55098591164a09e229e7a879ee
MasindeOdera <MasindeOdera@gmail.com>
2020-05-26 17:21:18 +0200

    Grouped episodes by season

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -5,14 +5,25 @@ import './EpisodeList.scss';
 function EpisodeList() {
     const state = useSelector( (state) => state);
     // const [first, setFirst] = useState([]);
+    // const [firstSeason, setfirstSeason] = useState([]);
     let episodes = state.list;
-    
+    let first, second, third;
     if(episodes) {
-        let firstSeason = episodes.filter(function(episode) {
+        first = episodes.filter(function(episode) {
             return episode.season === 1;
         });
-        console.log(firstSeason);
+
+        second = episodes.filter(function(episode) {
+            return episode.season === 2;
+        });
+
+        third = episodes.filter(function(episode) {
+            return episode.season === 3;
+        });
     }
+    console.log({first});
+    console.log({second});
+    console.log({third});
 
 
     return (
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,8 @@ function Navigation() {
     
     return (
         <div className="Navigation">
-            <button><NavLink to="/the-powerpuff-girls" activeClassName="button:hover" exact={true}><b>Main</b></NavLink></button>
-            <button><NavLink to="/the-powerpuff-girls/episodes" activeClassName="button:hover"><b>Episodes</b></NavLink></button>
+            <NavLink to="/the-powerpuff-girls" activeClassName="selected" exact={true}><button><b>Main</b></button></NavLink>
+            <NavLink to="/the-powerpuff-girls/episodes" activeClassName="selected"><button><b>Episodes</b></button></NavLink>
         </div>
     )
 }
